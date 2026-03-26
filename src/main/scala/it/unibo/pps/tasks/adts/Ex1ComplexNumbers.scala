324093b95194ef5adec0727a47d1393f3d0e0faa
Andrea Baldazzi <andrea.baldazzi7@studio.unibo.it>
2026-03-26 12:20:09 +0100

    ex1

---
+++ src/main/scala/it/unibo/pps/tasks/adts/Ex1ComplexNumbers.scala
@@ -0,0 +1,41 @@
+package it.unibo.pps.tasks.adts
+
+/*  Exercise 1: 
+ *  Complete the implementation of ComplexADT trait below, so that it passes
+ *  the test in ComplexTest.
+ */
+
+object Ex1ComplexNumbers:
+
+  trait ComplexADT:
+    type Complex
+
+    def complex(re: Double, im: Double): Complex
+
+    extension (complex: Complex)
+      def re(): Double
+      def im(): Double
+      def sum(other: Complex): Complex
+      def subtract(other: Complex): Complex
+      def asString(): String
+
+  object BasicComplexADT extends ComplexADT:
+    private case class ComplexImpl(re: Double, im: Double)
+
+    opaque type Complex = ComplexImpl
+
+    def complex(re: Double, im: Double): Complex = ComplexImpl(re, im)
+
+    extension (complex: Complex)
+      def re(): Double = complex match
+        case ComplexImpl(re, _) => re
+      def im(): Double = complex match
+        case ComplexImpl(_, im) => im
+      def sum(other: Complex): Complex = (complex, other) match
+        case (ComplexImpl(re1, im1), ComplexImpl(re2, im2)) => ComplexImpl(re1 + re2, im1 + im2)
+      def subtract(other: Complex): Complex = (complex, other) match
+        case (ComplexImpl(re1, im1), ComplexImpl(re2, im2)) => ComplexImpl(re1 - re2, im1 - im2)
+      def asString(): String = complex match
+        case ComplexImpl(re, 0) => s"$re"
+        case ComplexImpl(0, im) => s"${im}i"
+        case ComplexImpl(re, im) => s"$re ${if im >= 0.0 then "+" else "-"} ${math.abs(im)}i"
